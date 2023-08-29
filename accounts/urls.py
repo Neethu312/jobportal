55835a2b3c900417ea58dce24d9e2565a7f71611
Neethu <neethusuresh6211@gmail.com>
2023-08-29 21:17:08 +0530

    Company and User Model Created

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('company_register/', CompanyRegisterView.as_view()),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView, Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
+from serializers import *
 # Create your views here.
+
+
+class CompanyRegisterView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password1 = request.data.get('password1')
+        password2 = request.data.get('password2')
+        if password1 == password2:
+            custom_ser = CustomUserSerializer(
+                data={
+                    'username': username,
+                    'email': email,
+                    'password': password1,
+                    'role': 2
+                    }
+            )
+
+            if custom_ser.is_valid():
+                custom_ser.save()
+
+                company_id = custom_ser.data['id']
+                company = CustomUser.objects.filter(id=company_id)
+                company_ser = CompanyProfileSerializer(data=request.data)
+
+                if company_ser.is_valid():
+                    company_ser.save(fk_company=company)
+                    return Response(
+                    {
+                        'success': True,
+                        'data':  company_ser.data,
+                        'message': 'Company Successfully Registered ',
+                        'errors': None
+                    })
+                return Response(company_ser.errors)
+
+            return Response(custom_ser.errors)
+
+        return Response(
+            {
+                'success': False,
+                'data': None,
+                'message': 'Password Mismatch',
+                'errors': None
+            }
+        )
+
